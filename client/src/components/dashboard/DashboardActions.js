4eb1fbd7af7f96476c28ac27bff3684ed61b1442
kaydo1506 <okedoifeoma@yahoo.com>
2022-05-03 11:26:34 +0100

    prep for deploy

--- client/src/components/dashboard/DashboardActions.js
+++ client/src/components/dashboard/DashboardActions.js
@@ -10,12 +10,12 @@ const DashboardActions = () => {
             <Link to='/add-experience' className='btn btn-light'>
                 <i className='fab fa-black-tie text-primary' /> Add Experience
             </Link>
-            <Link to='/add-education' className='btn btn-light'>
-                <i className='fas fa-graduation-cap text-primary' /> Add
-                Education
-            </Link>
         </div>
     );
 };
 
 export default DashboardActions;
+
+//   <Link to='/add-education' className='btn btn-light'>
+//       <i className='fas fa-graduation-cap text-primary' /> Add Education
+//   </Link>;
