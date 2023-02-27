870b372621b0b302aaa8f0f48247ae49b786c4d0
kaydo1506 <okedoifeoma@yahoo.com>
2023-02-27 11:56:27 +0100

    new db

---
+++ client/build/precache-manifest.674bbd0dd0c855b05f686c2d7e6af07a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "745de73c85c152fa5ffb",
+    "url": "/static/css/main.765c62c3.chunk.css"
+  },
+  {
+    "revision": "745de73c85c152fa5ffb",
+    "url": "/static/js/main.745de73c.chunk.js"
+  },
+  {
+    "revision": "ee40437dfb8d7d89a0d6",
+    "url": "/static/js/1.ee40437d.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "4b31330b20e3cff3e49d540e4e7175e2",
+    "url": "/static/media/showcase.4b31330b.jpg"
+  },
+  {
+    "revision": "49f3fbbe422e3752088858c1e4f59e05",
+    "url": "/index.html"
+  }
+];
