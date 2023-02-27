944360a649c211204f99fda50f8b49fe860abb6d
kaydo1506 <okedoifeoma@yahoo.com>
2023-02-27 14:02:52 +0100

    hid secrets

--- server.js
+++ server.js
@@ -27,6 +27,6 @@ if (process.env.NODE_ENV === 'production') {
     });
 }
 
-const PORT = process.env.PORT || 4000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on ${PORT}`));
